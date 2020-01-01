5a79cbff99c1033bbc273edade5995e7b686cdba
isjaki <usow.aleksey@yandex.ru>
2020-01-01 20:59:05 +0300

    create Product component

---
+++ src/components/Product/Product.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import { Action } from 'typings/global';
+import { Seller } from 'typings/sellers';
+
+import ProductSellerInfo from 'components/Product/ProductSellerInfo/ProductSellerInfo';
+import ProductPicture from 'components/Product/ProductPicture/ProductPicture';
+import ProductPrice from 'components/Product/ProductPrice/ProductPrice';
+import Button from 'components/UI/Button/Button';
+
+import styles from 'components/Product/Product.scss';
+
+type Props = {
+    title: string;
+    price: number;
+    pictures: string[];
+    sellerInfo: Seller;
+    isProductFavorite: boolean;
+    onAddToFavoritesButtonClick: () => Action;
+};
+
+const Product = ({
+    title,
+    price,
+    pictures,
+    sellerInfo,
+    isProductFavorite,
+    onAddToFavoritesButtonClick,
+}: Props): JSX.Element => (
+    <div className={styles.product}>
+        <ProductPicture pictures={pictures} />
+        <div className={styles.mainInfo}>
+            <h2 className={styles.title}>{title}</h2>
+            <ProductPrice price={price} />
+            <ProductSellerInfo name={sellerInfo.name} rating={sellerInfo.rating} />
+        </div>
+        <Button
+            buttonType="addToFavorites"
+            isActive={isProductFavorite}
+            tooltip="добавить в избранное"
+            onClick={onAddToFavoritesButtonClick}
+        />
+    </div>
+);
+
+export default Product;
